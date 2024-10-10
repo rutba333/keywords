188c9af5a55119329b73edd926dd25eb14318626
rutba333 <sa2497589@gmail.com>
2024-10-10 21:00:43 +0600

    pass

---
+++ pass.py
@@ -0,0 +1,11 @@
+for x in range(10): #iterate loop
+    if x % 20==0: #condition 1
+     print("twist")
+    elif x% 15==0: #condition 2
+     pass
+    elif x% 5==0: #condition 3
+      print("fizz")
+    elif x%3==0: #condition 4
+      print("buzz")
+    else:
+      print(x)
