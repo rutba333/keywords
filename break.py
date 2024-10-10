97cb9b7d4267c3d8e56d8987aad5a22922b437e9
rutba333 <sa2497589@gmail.com>
2024-10-10 20:49:55 +0600

    break

---
+++ break.py
@@ -0,0 +1,10 @@
+#Take user input
+a=input("Enter a word in capital letter")
+#program to check break keyword
+for i in a: #itreate loop
+ if (i=='A'):
+  #display result
+    print("A is found")
+    break #break statement
+else:
+   print("A is not found")
