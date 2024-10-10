62e20bf0e2839caaec5cfdc5ded51460b756d151
rutba333 <sa2497589@gmail.com>
2024-10-10 21:21:57 +0600

    chatbot

---
+++ chatbot.py
@@ -0,0 +1,18 @@
+print("hello!This is zannat. ")
+name=input("what's your name?")
+print(f"Nice to meet you {name}.")
+friends=input("Can we be friends?\n yes or no")
+if friends=='yes':
+    print("Okayy my dear friend")
+else:
+    print("oh now I'm sad dear.")
+
+colour=input("what's your favourite colour dear?")
+print("oh! it's my favourite colour too")
+day=input("so how was your day?")
+if day=='good':
+    print("oh niceee")
+else:
+    print("Hope tommorow it will be great")
+hobby=input("what's your hobby?")
+print(f"woww {hobby} is a nice hobby.")
